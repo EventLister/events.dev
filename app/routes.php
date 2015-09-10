f779f2743067f0c5c0859ff150d1d983a1e9d5f1
Remington <remingtonwilliams@gmail.com>
2015-09-10 15:29:00 -0500

    deletes

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,6 @@
 |
 */
 
-Route::resource('posts', 'PostsController');
 
 
 Route::get('/', function()
--- app/views/posts/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-@section('title')
-
-@stop
-@section('content')
-/vagrant/sites/events.dev/app/views/welcome.blade.php
-
-
-@stop
