22fc55c1d382d59673f0d4418538d837aba2fd33
Johnathan Christopherson <johnathan.christopherson@live.com>
2015-09-11 14:55:58 -0500

    push

--- app/views/events/partials/create_event-form.blade.php
+++ app/views/events/partials/create_event-form.blade.php
@@ -1,15 +1,25 @@
 
 <div class="well colors col-sm-offset-2 col-sm-8 col-md-8">
 
-	<div class="form-group @if($errors->has('title')) has-error @endif">
-		{{ Form::label('title', 'Title') }}
-		{{ Form::text('title', null, ['class' => 'form-control'])}}
+	<div class="form-group @if($errors->has('event_name')) has-error @endif">
+		{{ Form::label('event_name', 'Event Name') }}
+		{{ Form::text('event_name', null, ['class' => 'form-control'])}}
 	</div>
 
-	<div class="form-group @if($errors->has('body')) has-error @endif">
-		{{ Form::label('body', 'Body') }}
-		{{ Form::textarea('body', null, ['class' => 'form-control'])}}
+	<div class="form-group @if($errors->has('event_description')) has-error @endif">
+		{{ Form::label('event_description', 'Event Description') }}
+		{{ Form::textarea('event_description', null, ['class' => 'form-control'])}}
 	</div>
+
+	<div class="col-sm-6 form-group @if($errors->has('event_location')) has-error @endif">
+	    {{ Form::label('event_location', 'Event Location') }}
+	    {{ Form::text('event_location', null, ['class' => 'form-control'])}}
+    </div>
+
+    <div class="col-sm-6 form-group @if($errors->has('event_time')) has-error @endif">
+        {{ Form::label('event_time', 'Event Time') }}
+        {{ Form::text('event_time', null, ['class' => 'form-control'])}}
+    </div>
 
 	<div>
 		<button class="btn btn-primary pull-left"> Create Event </button>
