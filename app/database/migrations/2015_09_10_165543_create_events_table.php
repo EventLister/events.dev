0e781d428195a8688fd674d2da8d6efcc608a8aa
Johnathan Christopherson <johnathan.christopherson@live.com>
2015-09-11 09:39:45 -0500

    table fixes

--- app/database/migrations/2015_09_10_165543_create_events_table.php
+++ app/database/migrations/2015_09_10_165543_create_events_table.php
@@ -20,6 +20,8 @@ class CreateEventsTable extends Migration {
 			$table->string('event_location');
 			$table->dateTime('event_time');
 			$table->string('time_zone');
+			$table->integer('user_id')->unsigned();
+    		$table->foreign('user_id')->references('id')->on('users');
 
 			$table->timestamps();
 		});
