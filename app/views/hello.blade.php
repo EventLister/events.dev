ee9bb4966e50ea4a5e485f0362ad6747d961dba4
Remington <remingtonwilliams@gmail.com>
2015-09-11 11:10:18 -0500

    form

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -6,11 +6,16 @@
 	</head>
 	<body>
 		<header>
-			
+			<!-- Latest compiled and minified CSS -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+			<!-- Optional theme -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
 		</header>
 		<main>
 			<div class="container">
-				<div class="well">
+				<div class="well" id="form">
 					{{ Form::open(array('action' => 'EventsController@storeUser')) }}
 						@include('events.partials.create_user_form')
 					{{Form::close()}}
@@ -18,7 +23,9 @@
 			</div>
 		</main>
 		<footer>
-			
+			<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+		    <!-- Latest compiled and minified JavaScript -->
+		    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		</footer>
 	</body>
 </html>
