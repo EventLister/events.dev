c33f53ede53a9aa23f8f43e8ed83d6a142edbf2a
Johnathan Christopherson <johnathan.christopherson@live.com>
2015-09-11 11:27:18 -0500

    time zone

--- app/database/migrations/2015_09_10_181247_create_users_table.php
+++ app/database/migrations/2015_09_10_181247_create_users_table.php
@@ -26,7 +26,7 @@ class CreateUsersTable extends Migration {
 		 		$table->string('state', 255);
 		 		$table->string('zip_code', 255);
 		 		$table->string('phone', 255);
-		 		$table->string('time_zone')->nullable();
+		 		$table->string('time_zone');
 
 
 			$table->rememberToken();
