35a4c34166facfb5a0bbd6a2803322370d8e967c
Remington <remingtonwilliams@gmail.com>
2015-09-11 15:15:30 -0500

    master layout changes

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -26,9 +26,14 @@
             </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
+            @if(Auth::check())
+                <li><a href="{{{action('HomeController@doLogout')}}}">Logout</a></li>
+            @else
+                <li><a href="{{{action('HomeController@showLogin')}}}">Login</a></li>
+            @endif
           </ul>
         </div><!--/.nav-collapse -->
       </div>
