f762ec2ea55cdaddec1058da297bc291e5483f1f
Johnathan Christopherson <johnathan.christopherson@live.com>
2015-09-17 13:42:28 -0500

    update

--- app/routes.php
+++ app/routes.php
@@ -10,6 +10,7 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+Route::get('otherProfile/{id}', 'EventsController@otherProfile'); 
 Route::get('attend/{id}', 'EventsController@attend');
 Route::get('profile', 'EventsController@userProfile'); 
 Route::get('showevents', 'HomeController@showEvents'); 
